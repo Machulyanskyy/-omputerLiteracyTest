1ae064c038a20d5d2a625351c8a6b4d88ea81cc7
machuyanskyi_d <machuyanskyi_d@ZKT07944.rada.gov.ua>
2016-04-15 11:06:12 +0300

    Добавил папки для скриптов и стилей

---
+++ application/views/dashboard.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard</title>
+    </head>
+    <body>
+        <form id="create-test">
+            
+        </form>
+    </body>
+    <footer>
+        <script src="application/scripts/jquery-2.2.3.min.js"></script>
+        <script src="application/scripts/dashboard.js"></script>
+    </footer>
+</html>
